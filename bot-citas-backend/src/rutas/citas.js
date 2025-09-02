f364c148d5d14766a27cb8149e27b031c5ea645f
Oscar Fernández <141870241+ojen0k@users.noreply.github.com>
2025-09-02 18:18:23 -0400

    Delete bot-citas-backend directory
    Cambio de backend

--- bot-citas-backend/src/rutas/citas.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { crearCita, actualizarEstadoCita, listarCitas } from "../controladores/citaControlador.js";
-
-const router = Router();
-router.post("/", crearCita);
-router.patch("/:id/estado", actualizarEstadoCita);
-router.get("/", listarCitas);
-
-export default router;
