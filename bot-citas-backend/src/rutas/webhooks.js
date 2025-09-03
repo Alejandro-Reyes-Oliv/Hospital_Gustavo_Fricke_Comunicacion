22903686b9a20a8a64a385187366c2ed55940591
Noothowl <martinchipoco@gmail.com>
2025-09-02 20:01:51 -0400

    Refactor estructura

--- bot-citas-backend/src/rutas/webhooks.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { verificarWebhook, recibirWebhook } from "../controladores/webhookControlador.js";
-
-const router = Router();
-router.get("/whatsapp", verificarWebhook);
-router.post("/whatsapp", recibirWebhook);
-
-export default router;
