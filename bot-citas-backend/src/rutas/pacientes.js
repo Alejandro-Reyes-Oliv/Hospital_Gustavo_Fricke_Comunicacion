62437724d3194ddb0dc54a67f248832e40a851aa
Noothowl <martinchipoco@gmail.com>
2025-09-02 20:01:51 -0400

    Refactor estructura

--- bot-citas-backend/src/rutas/pacientes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { crearPaciente, listarPacientes } from "../controladores/pacienteControlador.js";
-
-const router = Router();
-router.post("/", crearPaciente);
-router.get("/", listarPacientes);
-
-export default router;
--- bot-citas-backend/src/servidor.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { conectarBD } from "./configuracion/bd.js";
-import app from "./app.js";
-
-const PUERTO = process.env.PUERTO || 4000;
-
-await conectarBD(process.env.MONGO_URI);
-app.listen(PUERTO, () => console.log(`🚀 Servidor listo en http://localhost:${PUERTO}`));
